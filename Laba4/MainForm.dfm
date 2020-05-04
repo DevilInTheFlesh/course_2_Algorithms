8fabaf21e9333fad7b271f4703bc697cf9ae6bc5
Max <62742586+DevilInTheFlesh@users.noreply.github.com>
2020-05-04 10:11:47 +0300

    Add files via upload

---
+++ Laba4/MainForm.dfm
@@ -0,0 +1,57 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 346
+  ClientWidth = 490
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poDesktopCenter
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Image1: TImage
+    Left = 0
+    Top = 64
+    Width = 489
+    Height = 289
+  end
+  object Edit1: TEdit
+    Left = 24
+    Top = 24
+    Width = 65
+    Height = 21
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 112
+    Top = 22
+    Width = 75
+    Height = 25
+    Caption = #1044#1086#1073#1072#1074#1080#1090#1100
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 224
+    Top = 22
+    Width = 75
+    Height = 25
+    Caption = #1054#1095#1080#1089#1090#1080#1090#1100
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 368
+    Top = 22
+    Width = 99
+    Height = 25
+    Caption = #1054#1090#1089#1086#1088#1090#1090#1080#1088#1086#1074#1072#1090#1100
+    TabOrder = 3
+    OnClick = Button3Click
+  end
+end
---
+++ Laba4/MainForm.pas
@@ -0,0 +1,61 @@
+unit MainForm;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Sort, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Edit1: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  mass : mas;
+  ItemCount : integer;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if Edit1.Text<>'' then
+    begin
+      inc(ItemCount);
+      mass[ItemCount].key:= StrToInt(Edit1.Text);
+      Edit1.Text:='';
+      printMas(mass,1,ItemCount,Image1);
+    end
+  else
+    ShowMessage('¬ведите значение');
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  Image1.Picture:=nil;
+  ItemCount:=0;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  SortMas(mass,1,ItemCount,Image1);
+end;
+
+Initialization
+  ItemCount:=0;
+
+end.
